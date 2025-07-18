a9723a13a41c77845365df952fa7b208a10f7eee
SonaliMali17 <sonumali78174@gmail.com>
2025-07-18 20:43:55 +0530

    Multiplication Program

---
+++ Assignment 1/4.Multyof two no.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int No1=0,No2=0,No3=0;
+    printf("Enter Numbers :");
+    scanf("%d%d",&No1,&No2);
+
+    No3 = No1*No2;
+
+    printf("The Multy is %d.",No3);
+
+    getch();
+    return 0;
+}
